e74e9dcb54c4578452ffa381d7647840daf510eb
Ashraful Islam <ashraful@field.buzz>
2019-11-07 17:09:58 +0600

    Added a basic sarpoka app with route(not implemented yet)

--- example.py
+++ example.py
@@ -2,9 +2,9 @@ from wsgiref.simple_server import make_server
 from sarpoka import render, route
 
 
-
 @route(path='/')
 def foo():
     return render('<h1>Working 2</h1>')
+
 
 foo()
--- sarpoka.py
+++ sarpoka.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 from wsgiref.simple_server import make_server
 
+
 def route(**kwargs):
     def decorator(func, path='/', **kwargs):
         return run(web_app=func, path=path)
+
     return decorator
+
 
 def render(data):
     return data.encode()
 
+
 def run(web_app, path='/'):
     def app_wrapper(env, response):
+        request_path = env.get('PATH_INFO')
         status = '200 OK'
-        headers = [('Content-Type', 'text/html; charset=utf-8'),]
+        headers = [('Content-Type', 'text/html; charset=utf-8'), ]
         response(status, headers)
-        return [web_app()]
-    with make_server('', 8001, app_wrapper) as httpd:
-        print('Servin on http://127.0.0.1:8001')
+        if request_path == path:
+            return [web_app()]
+        return [b'<h2>404 not found</h2>']
+
+    with make_server('', 8002, app_wrapper) as httpd:
+        print('Serving on http://127.0.0.1:8002')
         httpd.serve_forever()
